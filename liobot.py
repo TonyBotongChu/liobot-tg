2bc680bb0e7377e2fcba1849b4b8b65c2b9cbb2b
zbt <zbt945263008@buaa.edu.cn>
2021-09-10 16:41:15 +0800

    introduce bot backend to tg bot

--- liobot.py
+++ liobot.py
@@ -5,6 +5,8 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 
 import yaml
 
+from app import BotBackend
+
 with open("bot.yaml", 'r') as stream:
     settings = yaml.safe_load(stream)
 
@@ -20,6 +22,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+bot_backend = BotBackend("ernie-1.0")
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -38,8 +41,22 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 
 def answer(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    template = ["您天天都在", (2, 5), "，您完全", (4, 7), "是吗？"]
+    text = bot_backend.fill_template(template, update.message.text)
+    text = bot_backend.fill_mask(text)
+    update.message.reply_text(text)
+
+
+def fill_template(update: Update, context: CallbackContext) -> None:
+    template_name = context.args[0]
+    text = context.args[1]
+    if template_name == "健身环":
+        template = ["您每天都在", (2, 5), "，您", (4, 7), "是吗？"]
+    else:
+        template = ["失败"]
+    text = bot_backend.fill_template(template, text)
+    text = bot_backend.fill_mask(text)
+    update.message.reply_text(text)
 
 
 def main() -> None:
@@ -56,8 +73,10 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
+    dispatcher.add_handler(CommandHandler("say", fill_template))
+
     # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer))
+    # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer))
 
     # Start the Bot
     updater.start_polling()
