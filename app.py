06ab1617e542b14a5af5236a1594875847a042e9
zbt <zbt945263008@buaa.edu.cn>
2022-01-13 17:08:59 +0800

    use pipeline instead

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import numpy
 import torch
 import random
-from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+from torch import nn
+from transformers import AutoTokenizer, AutoModelForMaskedLM, FillMaskPipeline
 
 import logging
 
@@ -8,55 +11,32 @@ class BotBackend:
     def __init__(self, model_name):
         self.tokenizer = AutoTokenizer.from_pretrained("model/" + model_name)
         self.model = AutoModelForMaskedLM.from_pretrained("model/" + model_name)
-
-    def fill_template(self, template, word):
-        masked_text = ""
-        masked_text += self.tokenizer.cls_token
-        word_not_filled = word is not None
-        for element in template:
-            if isinstance(element, str):
-                masked_text += element
-            else:
-                if word_not_filled:
-                    masked_text += word
-                    word_not_filled = False
-                    continue
-                min_len, max_len = element
-                len = random.randint(min_len, max_len)
-                masked_text += self.tokenizer.mask_token * len
-        masked_text += self.tokenizer.sep_token
-        return masked_text
+        self.fm_pipeline = FillMaskPipeline(model=self.model, tokenizer=self.tokenizer)
 
     # TODO: add banned or excluded words here
     def fill_mask(self, text):
         while self.tokenizer.mask_token in text:
-            top_num = text.count(self.tokenizer.mask_token) + 1
-            logging.info("top num = " + str(top_num))
-            input = self.tokenizer.encode(text, return_tensors="pt")
-            mask_token_indexes = torch.where(input == self.tokenizer.mask_token_id)[1]
-            # mask_token_index = torch.where(input == tokenizer.mask_token_id)[1]
-            mask_token_index = torch.reshape(random.choice(mask_token_indexes), (1,))
-            token_logits = self.model(input)[0]
-            mask_token_logits = token_logits[0, mask_token_index, :]
-            top_k_tokens = torch.topk(mask_token_logits, top_num, dim=1).indices[0].tolist()
-            # predict_token = torch.topk(mask_token_logits, 1, dim=1).indices[0].tolist()[0]
-            predict_token = random.choice(top_k_tokens)
-            # predict_word = self.tokenizer.decode([predict_token])
-            output = input.clone()
-            # current_token = output[0][mask_token_index.tolist()[0]].tolist()
-            output[0][mask_token_index.tolist()[0]] = predict_token
-            text = self.tokenizer.convert_ids_to_tokens(output[0])
-            # remove redundant [CLS] and [SEP]
-            text = text[1:-1]
-            text = ''.join(text)
-            logging.info(text)
-        if text.startswith(self.tokenizer.cls_token):
-            text = text[len(self.tokenizer.cls_token):]
-        if text.endswith(self.tokenizer.sep_token):
-            text = text[:-len(self.tokenizer.sep_token)]
-        logging.info("text complete")
-        return text
+            result = self.fm_pipeline(text)
+            if text.count(self.tokenizer.mask_token) == 1:
+                text = result[0]['sequence']
+            else:
+                # the first index represents which <mask> to be filled
+                # and the second index represents the candidate number, sort by score
+                mask_to_fill = random.choice(result)
+                scores = torch.zeros(len(mask_to_fill))
+                for i in range(0, len(mask_to_fill)):
+                    scores[i] = mask_to_fill[i]['score']
+                s = nn.Softmax(dim=0)
+                probabilities = s(scores)
 
+                # numpy bug: raise 'ValueError: probabilities do not sum to 1' even if the sum is 1
+                probabilities = numpy.asarray(probabilities).astype('float64')
+                probabilities = probabilities / numpy.sum(probabilities)
+
+                print(probabilities)
+                text = numpy.random.choice(mask_to_fill, p=probabilities)['sequence']
+        text = text.replace(' ', '')
+        return text
 
 if __name__ == "__main__":
     # bot_backend = BotBackend("guwenbert-large")
